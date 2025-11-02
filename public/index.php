12b04f7ec872c5ef84f68b865d98ee941fd062f2
Andre Pinto <andrepinto.cg@gmail.com>
2025-11-02 02:25:39 -0300

    feat: first endpoint - hello world

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response){
+
+    $response->getBody()->write("Olá mundo!");
+
+    return $response;
+});
+
+$app->run();
